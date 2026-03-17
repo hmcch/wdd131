ada3b412107b807ad0cba6215c5f3a1fc6ccc948
hmcch <hcastrochvez@byupathway.edu>
2026-03-17 13:26:09 -0600

    js li added to hold each entry chapter title and associated delete button

--- week02/scripts/script.js
+++ week02/scripts/script.js
@@ -1,3 +1,4 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('______'); // Fill in the blank to reference the unordered list element.
+const li = document.createElement('li');
