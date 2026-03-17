6b280a51a3918f46b3e27c0ae79d11b806dc2b55
hmcch <hcastrochvez@byupathway.edu>
2026-03-17 13:38:44 -0600

    Set the delete button's textContent to ❌ Set the delete button's textContent

--- week02/scripts/script.js
+++ week02/scripts/script.js
@@ -3,4 +3,5 @@ const button = document.querySelector('button');
 const list = document.querySelector('______'); // Fill in the blank to reference the unordered list element.
 const li = document.createElement('li');// the variable name does not have to match the element type being created.
 const deleteButton = document.createElement('button');//create delete button
-li.textContent = input.value;
+li.textContent = input.value;//Populate the li element variable's textContent or innerHTML with the input value
+deleteButton.textContent = '❌';// Set the delete button's textContent to ❌
