9defca274492fa4e12967314c1b2e76fcdcf2c10
hmcch <hcastrochvez@byupathway.edu>
2026-03-17 13:28:53 -0600

    js delete button created

--- week02/scripts/script.js
+++ week02/scripts/script.js
@@ -1,4 +1,5 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('______'); // Fill in the blank to reference the unordered list element.
-const li = document.createElement('li');
+const li = document.createElement('li');// the variable name does not have to match the element type being created.
+const deleteButton = document.createElement('button');//create delete button
