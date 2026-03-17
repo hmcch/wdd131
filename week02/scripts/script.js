253954c7a132021ecc56d9d5b4980926770005cf
hmcch <hcastrochvez@byupathway.edu>
2026-03-17 13:32:53 -0600

    js populate li element variable textContent with the input value

--- week02/scripts/script.js
+++ week02/scripts/script.js
@@ -3,3 +3,4 @@ const button = document.querySelector('button');
 const list = document.querySelector('______'); // Fill in the blank to reference the unordered list element.
 const li = document.createElement('li');// the variable name does not have to match the element type being created.
 const deleteButton = document.createElement('button');//create delete button
+li.textContent = input.value;
