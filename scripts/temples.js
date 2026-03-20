26e8dd049dba4d3d0c40acf37bd88f1817b95289
hmcch <hcastrochvez@byupathway.edu>
2026-03-19 20:31:40 -0600

    js functionality added to display hamburguer, close it

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,20 @@
+const menuButton = document.getElementById('navigation');
+const navigation = document.querySelector('.navigation-menu');
+const lastModified = document.querySelector('#lastModified');
+const currentYar = document.getElementById('#currentyear');
+
+menuButton.addEventListener('click', () => {
+    if (navigation.classList.contains('open')) {
+        navigation.classList.remove('open');
+        menuButton.textContent = '☰'
+    }
+
+    else {
+        navigation.classList.add('open');
+        menuButton.textContent = 'X';
+
+    }
+});
+
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = document.lastModified;
