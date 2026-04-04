8324e87b240e93fd80fdd4b35d6c5e9b362748c4
hmcch <hcastrochvez@byupathway.edu>
2026-04-04 17:32:38 -0600

    js filtered temples alt caption added for all temples

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -17,7 +17,8 @@ const temples = [
         dedicated: "2005, August, 7",
         area: 11500,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg",
+        alt: "Nigeria Temple",
     },
     {
         templeName: "Manti Utah",
@@ -25,7 +26,8 @@ const temples = [
         dedicated: "1888, May, 21",
         area: 74792,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg",
+        alt: "Manti Temple",
     },
     {
         templeName: "Payson Utah",
@@ -33,7 +35,8 @@ const temples = [
         dedicated: "2015, June, 7",
         area: 96630,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg",
+        alt: "Payson Temple",
     },
     {
         templeName: "Yigo Guam",
@@ -41,7 +44,8 @@ const temples = [
         dedicated: "2020, May, 2",
         area: 6861,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg",
+        alt: "Yigo Temple",
     },
     {
         templeName: "Washington D.C.",
@@ -49,7 +53,8 @@ const temples = [
         dedicated: "1974, November, 19",
         area: 156558,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg",
+        alt: "Washington D.C. Temple",
     },
     {
         templeName: "Lima Perú",
@@ -57,23 +62,26 @@ const temples = [
         dedicated: "1986, January, 10",
         area: 9600,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg",
+        alt: "Lima Temple",
     },
     {
-        templeName: "Mexico City Mexico",
+        templeName: "Mexico City",
         location: "Mexico City, Mexico",
         dedicated: "1983, December, 2",
         area: 116642,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg",
+        alt: "Mexico City Temple",
     },
     {
-        templeName: "Tuxtla Gutierrez Mexico",
+        templeName: "Chiapas Mexico",
         location: "Tuxtla Gutierrez, Chiapas, Mexico",
         dedicated: "2000, March, 12",
         area: 10700,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/tuxtla-gutierrez-mexico-temple/tuxtla-gutierrez-mexico-temple-15226-main.jpg"
+            "https://churchofjesuschristtemples.org/assets/img/temples/tuxtla-gutierrez-mexico-temple/tuxtla-gutierrez-mexico-temple-15226-main.jpg",
+        alt: "Tuxtla Gutierrez Temple",
     },
     {
         templeName: "Merida Mexico",
@@ -81,15 +89,17 @@ const temples = [
         dedicated: "2000, July, 8",
         area: 10700,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/merida-mexico-temple/merida-mexico-temple-14119-main.jpg"
+            "https://churchofjesuschristtemples.org/assets/img/temples/merida-mexico-temple/merida-mexico-temple-14119-main.jpg",
+        alt: "Merida Temple",
     },
     {
-        templeName: "Puebla, Mexico",
+        templeName: "Puebla Mexico",
         location: "Puebla, Puebla, Mexico",
         dedicated: "2024, May, 19",
         area: 35865,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/puebla-mexico-temple/puebla-mexico-temple-46342-main.jpg"
+            "https://churchofjesuschristtemples.org/assets/img/temples/puebla-mexico-temple/puebla-mexico-temple-46342-main.jpg",
+        alt: "Puebla Temple",
     },
 ];
 
