be3a3d6b5ed0bb1f1796bd70b79192e752ef57b6
hmcch <hcastrochvez@byupathway.edu>
2026-04-07 19:11:06 -0600

    js arrays added

---
+++ scripts/form.js
@@ -0,0 +1,53 @@
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+const productSelect = document.getElementById("productName");
+
+if (productSelect) {
+    products.forEach(product => {
+        const option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name;
+        productSelect.appendChild(option);
+    });
+}
+
+const lastModified = document.getElementById("lastModified");
+
+if (lastModified) {
+    lastModified.textContent = `Last Modified: ${document.lastModified}`;
+}
+
+const reviewCount = document.getElementById("reviewCount");
+
+if (reviewCount) {
+    let count = Number(localStorage.getItem("reviewCount")) || 0;
+    count++;
+    localStorage.setItem("reviewCount", count);
+    reviewCount.textContent = `Reviews completed: ${count}`;
+}
